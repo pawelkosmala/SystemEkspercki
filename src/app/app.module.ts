1054770f086b9f601b13aa1c17cdf4024f4c1432
Pueblo <pawel.kosmala94@gmail.com>
2019-01-26 19:34:17 +0100

    Added components: home, progress-bar, question, result, survey

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SurveyComponent } from './components/survey/survey.component';
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
+import { QuestionComponent } from './components/question/question.component';
+import { ResultComponent } from './components/result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SurveyComponent,
+    ProgressBarComponent,
+    QuestionComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule
