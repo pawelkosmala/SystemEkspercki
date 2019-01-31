a2c845c420aecf6028059f7ba4e6d0dfba77213a
Pueblo <pawel.kosmala94@gmail.com>
2019-01-31 21:48:32 +0100

    Added bootstrap and generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -7,6 +9,11 @@ import { SurveyComponent } from './components/survey/survey.component';
 import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 import { QuestionComponent } from './components/question/question.component';
 import { ResultComponent } from './components/result/result.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'survey', component: SurveyComponent }
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +25,9 @@ import { ResultComponent } from './components/result/result.component';
     ResultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
