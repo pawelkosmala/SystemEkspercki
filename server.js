53ba4c834dd3cd66bd0b08806ce74df654431a76
Dominika Kawucha <dominika.kawucha@icloud.com>
2019-02-03 16:10:16 +0100

    Weird routing problem

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.use(express.static('./dist/sysExpRes'));
 app.get('/', function(req,res) { 
     res.sendFile(path.join(__dirname, '/dist/sysExpRes/index.html'));
 });
+app.get('/survey', function(req,res) { 
+    res.sendFile(path.join(__dirname, '/dist/sysExpRes/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
