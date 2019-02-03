14bbe54cfdba1c343c54282ac8d88083a3c06f9e
Dominika Kawucha <dominika.kawucha@icloud.com>
2019-02-03 16:20:11 +0100

    maybe this time

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/sysExpRes'));
-app.get('/', function(req,res) { 
-    res.sendFile(path.join(__dirname, '/dist/sysExpRes/index.html'));
-});
-app.get('survey', function(req,res) { 
+app.get('*', function(req,res) { 
     res.sendFile(path.join(__dirname, '/dist/sysExpRes/index.html'));
 });
 
